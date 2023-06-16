e724c73b8a486b7f78ce1c0eb8d19a78f7489ec7
Salim Karim <imsjk8@gmail.com>
2023-06-16 21:58:14 +1000

    v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+
+  const handleInputChangeA = (event) => {
+    const bb = btoa(event.target.value);
+    setA(bb);
+  };
+
+  const handleInputChangeB = (event) => {
+    try {
+      const aa = atob(event.target.value);
+      setB(aa);
+    } catch (e) {
+      setB('Invalid base64 string');
+    }
+  };
+
+  const handleClearA = () => {
+    setA('');
+    document.getElementById('encode').value = '';
+  };
+
+  const handleClearB = () => {
+    setB('');
+    document.getElementById('decode').value = '';
+  };
+
+  const handleCopyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    toast.success('Copied to clipboard!', {
+      position: toast.POSITION.BOTTOM_CENTER
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Baser</h1>
+
+        <h3>Encode</h3>
+        <input id="encode" type="text" onChange={handleInputChangeA} />
+        <button onClick={handleClearA}>Clear</button>
+
+        <span
+          onClick={() => handleCopyToClipboard(a)}
+          style={{ cursor: 'pointer' }}
         >
-          Learn React
-        </a>
+          {a}
+        </span>
+
+        <h3>Decode</h3>
+        <input id="decode" type="text" onChange={handleInputChangeB} />
+        <button onClick={handleClearB}>Clear</button>
+
+        <span
+          onClick={() => handleCopyToClipboard(b)}
+          style={{ cursor: 'pointer' }}
+        >
+          {b}
+        </span>
+
+        <ToastContainer />
       </header>
     </div>
   );
